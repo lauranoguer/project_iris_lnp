37bc2756220bd91b75769eef9c0111cad37446bb
laura.noguer <laura.perez.n@gmail.com>
2024-02-06 12:14:33 +0100

    going back

--- main_script.R
+++ main_script.R
@@ -1,1 +1,11 @@
+library(dplyr)
+library(gglpot2)
+library(readr)
+
+# reading data
+iris_data <- read_csv('raw_data/iris.csv')
+
+# subsetting data
+setosa_subset <- iris_data |>
+  filter(Species == "setosa")
 iris_data <- iris
